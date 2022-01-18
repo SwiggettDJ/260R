9c7e46d37ba58d2a99f437cf7ea8f5eb05f611ff
DJ <swiggettdj@gmail.com>
2022-01-18 15:19:25 -0700

    Started working on spell particles

---
+++ Wizard Game/Assets/SpellBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellBase : MonoBehaviour
+{
+    private float spellDamage;
+    private ParticleSystem spellEffect;
+}
