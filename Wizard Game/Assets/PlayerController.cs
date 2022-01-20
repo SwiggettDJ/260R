12bde0bd6778a4bf41c70baeae0f32c67cd4fb09
DJ <swiggettdj@gmail.com>
2022-01-20 01:25:05 -0700

    Fixed movement and made top down camera

--- Wizard Game/Assets/PlayerController.cs
+++ Wizard Game/Assets/PlayerController.cs
@@ -1,24 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
     public CharacterController controller;
+    public Rigidbody rb;
 
-    public float speed = 5f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private Camera cam;
+
+    private float horizontal;
+    private float vertical;
+    private Vector3 moveDirection;
+
+    private Vector3 mousePos;
     
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
 
-        if (direction.magnitude >= 0.1f)
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
         {
-            controller.Move(direction * speed * Time.deltaTime);
+            mousePos = raycastHit.point;
         }
+        
+        moveDirection = new Vector3(horizontal, 0f, vertical);
+
+        Vector2 lookDirection = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        transform.LookAt(new Vector3(mousePos.x,transform.position.y,mousePos.z));
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDirection * speed;
     }
 }
